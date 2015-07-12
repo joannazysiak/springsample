8af4255ea626949df68cecd2b2661044bebc5977
Joanna Zysiak <joanna.zysiak@gmail.com>
2015-07-12 21:38:50 +0200

    springsample - empty project

---
+++ src/main/java/pl/joannazysiak/springsample/SpringsampleApplication.java
@@ -0,0 +1,12 @@
+package pl.joannazysiak.springsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsampleApplication.class, args);
+    }
+}

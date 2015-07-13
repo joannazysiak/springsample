8f056099efc5938f7b0088147344ebf8741e7a13
Joanna Zysiak <joannazysiak@wp.pl>
2015-07-13 23:30:48 +0200

    springsample - profiles

--- src/main/java/pl/joannazysiak/springsample/service/MockMailSender.java
+++ src/main/java/pl/joannazysiak/springsample/service/MockMailSender.java
@@ -2,9 +2,11 @@ package pl.joannazysiak.springsample.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("dev")
 public class MockMailSender implements MailSender {
 
 	private static final Log LOG = LogFactory.getLog(MockMailSender.class);
